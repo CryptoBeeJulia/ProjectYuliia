c5f06d480495ad1f9f50addb4c367790c52b4536
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2023-12-12 23:59:33 +0200

    filtered array with img done

--- yuliia.js
+++ yuliia.js
@@ -7,23 +7,53 @@ const options = {
 	}
 };
 
+
+
 showResults();
 async function showResults()
 {
 	const response = await fetch(url, options);
 	const respData = await response.json();
-	console.log(respData.result['vehicles']);
+    const respCars = respData.result['vehicles'];
+    //console.log(respCars);
+    const filteredArray = filterObjectsWithImages(respCars.slice(0, 45));
+
+    console.log(filteredArray);
+  
 } 
 
-const apiData = ???;
+function filterObjectsWithImages(apiArray) {
+    const filteredArray = [];
+  
+    for (const apiObject of apiArray) {
+      // Check if the image property is not null
+      if (apiObject.image !== null) {
+        // Create a new object with the required properties
+        const newCar = {
+          id: apiObject.id,
+          name: apiObject.name,
+          image: apiObject.image
+        };
+  
+        // Add the new object to the filtered array
+        filteredArray.push(newCar);
+      }
+    }
+  
+    return filteredArray;
+  }
+
+
 // I created a for loop to iterrate over the apiData
 //but I don't know how to assign the data that I see in console to the code 
 
+
+/*
 function  extractUsefulContent(data)
 {
     const filteredObjects = [];
     let i = 0;
-    for (apiData) {
+    for (respCars) {
         if (data.image !== null) {
             const newObj = {
                 id: data.id,
@@ -40,7 +70,7 @@ function  extractUsefulContent(data)
     return filteredObjects;
 }
 
-const filteredArray = extractUsefulContent(apiData);
+const filteredArray = extractUsefulContent(respCars);
 console.log(filteredArray);
 
 
@@ -57,13 +87,14 @@ function displayCars()
     cars.innerHTML = `
     <figure> <img src="${image}">
     <figcaption>${name}</figcaption>
-    <figcaption>$68</figcaption>
+    <figcaption>$72</figcaption>
     <button class="btn2">Buy now</button></figure>`
 
     gridContianer.appendChild(cars)
 }
+
+//same question here - to access data with ${} I need to name data 
+//same way as we had mealData in the exercise but I didn't find where we deckared mealData
 //for loop to have 6 items 
 
-
-
-
+*/
